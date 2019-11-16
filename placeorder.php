7ee718d092c227a6891f2ecdb8386c2d864d8cd5
unknown <ntboland@gmail.com>
2019-11-15 22:47:54 -0500

    placeorder styling fix

--- placeorder.php
+++ placeorder.php
@@ -1,6 +1,6 @@
 <?=template_header('Place Order')?>
 
-<div class="placeorder content-wrapper">
+<div class="container" style="padding: 150px 0px 150px 0px">
     <h1>Your Order Has Been Placed</h1>
     <p>Thank you for ordering with us, we'll contact you by email with your order details.</p>
 </div>
